acc253643c375bac404db6c4e44b41388e2bf937
mehdi lamaaffar <mehdilamaafar@gmail.com>
2017-11-09 04:34:00 +0000

    follow constant style

--- app/User.php
+++ app/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'is_admin'
+        'name',
+        'email',
+        'password',
+        'is_admin'
     ];
 
     /**
