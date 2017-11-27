a570ace273038466c9c794314c942f7560fdf17c
mehdi lamaaffar <mehdilamaafar@gmail.com>
2017-11-27 10:06:11 +0000

    refactor blog config

--- config/blog.php
+++ config/blog.php
@@ -1,17 +1,9 @@
 <?php
 
 return [
-    'name' => 'Me-Blog',
-    'about' => '
-        <p>Responsive web design offers us a way forward, finally allowing us to design for the ebb and flow of things. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly.</p>
-        <p>Responsive web design offers us a way forward, finally allowing us to design for the ebb and flow of things. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly.</p>
-        <div class="height-40px"></div>
-        <h2 class="title text-center">Social</h2>
-        <ul class="social">
-            <li class="facebook"><a href="#"><span class="ion-social-facebook"></span></a></li>
-            <li class="twitter"><a href="#"><span class="ion-social-twitter"></span></a></li>
-            <li class="google-plus"><a href="#"><span class="ion-social-googleplus"></span></a></li>
-            <li class="tumblr"><a href="#"><span class="ion-social-tumblr"></span></a></li>
-        </ul>
-    '
+    'name' => 'Me Blog',
+    'description' => ' Me\' s blog on PHP and Laravel',
+    'twitter_url' => 'https://twitter.com/mehdilamaaffar',
+    'github_url' => 'https://github.com/mehdilamaaffar',
+    'email_adress' => 'mehdilamaaafar@gmail.com',
 ];
